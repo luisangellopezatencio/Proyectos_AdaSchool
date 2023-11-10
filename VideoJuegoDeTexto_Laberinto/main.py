1f95c81463417c3ceb4f4d53fa4390ff400ed9b2
Luis A Lopez Atencio <luisangellopezatencio@outlook.com>
2023-11-10 15:55:46 -0500

    Agregada Parte 2 del proyecto

--- VideoJuegoDeTexto_Laberinto/main.py
+++ VideoJuegoDeTexto_Laberinto/main.py
@@ -1,4 +1,16 @@
+from readchar import readkey, key
+
 #Pedir el nombre del jugador
 name = input("Ingrese su nombre: ")
 #Imprimir el nombre
 print("Hola", name)
+
+#Iniciar el juego
+trigger = True
+while trigger:
+    print("Presione una tecla para continuar")
+    tecla = readkey()
+    print(f'Haz presionado la tecla {tecla}')
+    if tecla == key.UP:
+        print("Saliendo del juego")
+        trigger = False
