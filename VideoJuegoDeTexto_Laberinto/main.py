994c20d3d205ed3cf4dda7443457d437406272c7
Luis A Lopez Atencio <luisangellopezatencio@outlook.com>
2024-01-10 21:42:45 -0500

    Parte 3 del proyecto integrador

--- VideoJuegoDeTexto_Laberinto/main.py
+++ VideoJuegoDeTexto_Laberinto/main.py
@@ -1,4 +1,5 @@
 from readchar import readkey, key
+import os
 
 #Pedir el nombre del jugador
 name = input("Ingrese su nombre: ")
@@ -7,10 +8,26 @@ print("Hola", name)
 
 #Iniciar el juego
 trigger = True
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')  #Borrar la consola
+
+def increment():
+    global count
+    count = count + 1
+
+count = 0
 while trigger:
-    print("Presione una tecla para continuar")
+    print("Presione una tecla n para continuar")
     tecla = readkey()
-    print(f'Haz presionado la tecla {tecla}')
+    #print(f'Haz presionado la tecla {tecla}')
     if tecla == key.UP:
         print("Saliendo del juego")
         trigger = False
+    elif tecla == "n":
+        clear()
+        print(count)
+        increment()
+        if count > 50:
+            print("Saliendo del juego")
+            trigger = False
