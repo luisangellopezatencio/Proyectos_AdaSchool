2bc5d25c829c1be0bcba061611953341ef1e8a14
Luis A Lopez Atencio <luisangellopezatencio@outlook.com>
2024-01-11 18:34:53 -0500

    Parte 4 del proyecto integrador

--- VideoJuegoDeTexto_Laberinto/main.py
+++ VideoJuegoDeTexto_Laberinto/main.py
@@ -16,18 +16,91 @@ def increment():
     global count
     count = count + 1
 
-count = 0
-while trigger:
-    print("Presione una tecla n para continuar")
-    tecla = readkey()
-    #print(f'Haz presionado la tecla {tecla}')
-    if tecla == key.UP:
-        print("Saliendo del juego")
-        trigger = False
-    elif tecla == "n":
-        clear()
-        print(count)
-        increment()
-        if count > 50:
-            print("Saliendo del juego")
+def string_to_caracters_matrix(string: str):
+    """
+        Esta funcion convierte un string en una matriz de caracteres
+    """
+    return [list(line) for line in string.split("\n")]
+
+def clearAndShowMatrix(matrix):
+    """
+        Esta funcion limpia la consola y muestra la matriz
+    """
+    clear()
+    for line in matrix:
+        print("".join(line))
+
+def goDown(currentPoint):
+    currentPoint[0] = currentPoint[0] + 1
+    return currentPoint
+
+def goUP(currentPoint):
+    currentPoint[0] = currentPoint[0] - 1
+    return currentPoint
+
+def goLEFT(currentPoint):
+    currentPoint[1] = currentPoint[1] - 1
+    return currentPoint
+
+def goRIGHT(currentPoint):
+    currentPoint[1] = currentPoint[1] + 1
+    return currentPoint
+
+def mainLoop(map, initialPoint, finalPoint):
+    global trigger
+    currentPoint = list(initialPoint)
+    while trigger:
+        map[currentPoint[0]][currentPoint[1]] = "P"
+        clearAndShowMatrix(map)
+        previousPoint = currentPoint
+        map[previousPoint[0]][previousPoint[1]] = "."
+        key_ = readkey()
+        if key_ == key.UP:
+            if (map[currentPoint[0] - 1][currentPoint[1]] != "#"): #Verificamos que no haya una pared
+                currentPoint = goUP(currentPoint)
+        elif key_ == key.DOWN:
+            if map[currentPoint[0] + 1][currentPoint[1]] != "#":
+                currentPoint = goDown(currentPoint)
+        elif key_ == key.LEFT:
+            if map[currentPoint[0]][currentPoint[1] - 1] != "#":
+                currentPoint = goLEFT(currentPoint)
+        elif key_ == key.RIGHT:
+            if map[currentPoint[0]][currentPoint[1] + 1] != "#":
+                currentPoint = goRIGHT(currentPoint)
+        else:
             trigger = False
+        
+        
+
+laberinto = """..###################
+....#...............#
+#.#.#####.#########.#
+#.#...........#.#.#.#
+#.#####.#.###.#.#.#.#
+#...#.#.#.#.....#...#
+#.#.#.#######.#.#####
+#.#...#.....#.#...#.#
+#####.#####.#.#.###.#
+#.#.#.#.......#...#.#
+#.#.#.#######.#####.#
+#...#...#...#.#.#...#
+###.#.#####.#.#.###.#
+#.#...#.......#.....#
+#.#.#.###.#.#.###.#.#
+#...#.#...#.#.....#.#
+###.#######.###.###.#
+#.#.#.#.#.#...#.#...#
+#.#.#.#.#.#.#.#.#.#.#
+#.....#.....#.#.#.#.#
+###################.."""
+
+
+matrix_laberinto = string_to_caracters_matrix(laberinto)
+
+#Punto Inicial
+P0 = (0,0)
+#Punto Final
+Pf = (len(matrix_laberinto) - 1, len(matrix_laberinto[0]) - 1)
+
+#Ejecutar el juego
+mainLoop(matrix_laberinto, P0, Pf)
